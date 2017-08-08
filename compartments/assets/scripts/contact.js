45d82ffd2d62a74e201dfebb1125c82d2d5928d7
Te Vallee <astronomemusic@gmail.com>
2017-08-07 15:17:37 -1000

    required fields must be filled before submitting form

---
+++ compartments/assets/scripts/contact.js
@@ -0,0 +1,40 @@
+$.ajax({
+  url: "https://endtoend.airshipcms.io/api/aerostat_collections/373",
+  method: "GET",
+  success: function(data) {
+    console.log(data)
+    var collection = data;
+  }
+});
+
+function validateForm() {
+  var valid = true;
+  $(".required").each(function(i, field) {
+    var field = $(field)[0];
+    switch(field.tagName) {
+      case "INPUT":
+        if(field.type === "radio") {
+          if($("input[name='"+ $(field)[0].name + "']:checked")[0] === undefined) {
+            valid = false;
+          }
+        } else {
+          if(field.value === "") {
+            valid = false;
+          }
+        }
+        break;
+      default:
+        if(field.value === "") {
+          valid = false;
+        }
+        break;
+    }
+  });
+  if(valid) {
+    submit();
+  }
+}
+
+function submit() {
+  console.log('submitting')
+}
